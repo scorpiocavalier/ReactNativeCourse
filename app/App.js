94bdd94cf23dffbab6033aea5e1924744e2a0e57
Arturo Chen <art.can.fly@gmail.com>
2025-01-30 00:18:41 -0500

    Style layout of list and modal

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import "../global.css";
-import { createRef, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { useState } from "react";
+import { Text, Button, ScrollView, View } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [goals, setGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
   const addGoal = (goal) => setGoals((currentGoals) => [...currentGoals, goal]);
 
@@ -13,16 +14,26 @@ export default function App() {
     setGoals((currentGoals) => currentGoals.filter((_, i) => i !== index));
 
   return (
-    <View className="py-12 px-5 gap-5">
-      <GoalInput addGoal={addGoal} />
+    <View className="flex-1 pt-24 pb-6 px-5 gap-6">
+      <Text className="text-center text-3xl font-bold">My Goals</Text>
 
-      <ScrollView className="mb-7">
+      <ScrollView>
         <View className="gap-3">
           {goals.map((goal, index) => (
             <GoalItem key={index} goal={goal} removeGoal={() => removeGoal(index)} />
           ))}
         </View>
       </ScrollView>
+
+      <Button title="Add Goal" onPress={() => setModalIsVisible(true)} />
+
+      {modalIsVisible && (
+        <GoalInput
+          addGoal={addGoal}
+          closeModal={() => setModalIsVisible(false)}
+          isVisible={modalIsVisible}
+        />
+      )}
     </View>
   );
 }
--- app/components/GoalInput.js
+++ app/components/GoalInput.js
@@ -1,28 +1,40 @@
 import { useState } from "react";
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Button, Modal } from "react-native";
 
-export default function GoalInput({ addGoal }) {
+export default function GoalInput({ addGoal, closeModal, isVisible }) {
   const [inputText, setInputText] = useState("");
 
   const onAddGoal = () => {
     const goal = inputText.trim();
+
     if (inputText.length) {
       addGoal(goal);
       setInputText("");
+      closeModal();
     }
   };
 
   return (
-    <View className="flex-row justify-between items-center gap-3">
-      <TextInput
-        className="flex-1 border border-[#ccc] p-3"
-        placeholder="Your course goal"
-        value={inputText}
-        onChangeText={setInputText}
-        onSubmitEditing={onAddGoal}
-        submitBehavior="submit"
-      />
-      <Button title="Add Goal" onPress={onAddGoal} />
-    </View>
+    <Modal visible={isVisible} animationType="slide">
+      <View className="flex-1 justify-center gap-6 px-12">
+        <TextInput
+          className="border border-[#ccc] p-3"
+          placeholder="Your goal"
+          value={inputText}
+          onChangeText={setInputText}
+          onSubmitEditing={onAddGoal}
+          submitBehavior="submit"
+        />
+
+        <View className="flex-row gap-6">
+          <View className="flex-auto">
+            <Button title="Add Goal" onPress={onAddGoal} />
+          </View>
+          <View className="flex-auto">
+            <Button title="Cancel" onPress={closeModal} />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 }
